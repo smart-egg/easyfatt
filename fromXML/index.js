c96c15cb1336a180fe012b13a4266d0ddb097396
hector <c98hector@gmail.com>
2019-06-03 16:54:25 -0500

    finish xml2csv

--- fromXML/index.js
+++ fromXML/index.js
@@ -36,6 +36,7 @@ module.exports = (context, req) => {
 
                     temp = res.EasyfattDocuments.Documents[0].Document;
                     let documents = [];
+                    let orderID = '', orderDate = '', shipID = '', shipDate = '';
 
                     for (let index = 0; index < temp.length; index++) {
                         const item = temp[index];
@@ -45,7 +46,7 @@ module.exports = (context, req) => {
                             if (item.hasOwnProperty(key)) {
                                 switch (key) {
                                     case 'CostVatCode':
-                                        newEl['Invoice.' + key] = item[key][0]._;
+                                        newEl['Document.' + key] = item[key][0]._;
                                         break;
                                     case 'Payments':
                                         if (expand === 'payments') {
@@ -54,9 +55,16 @@ module.exports = (context, req) => {
                                                 for (let childIndex = 0; childIndex < childItems.length; childIndex++) {
                                                     let row = Object.assign({}, newEl);
                                                     const childItem = childItems[childIndex];
+                                                    row['PaymentRow.RowNumber'] = childIndex + 1;
+                                                    row['PaymentRow.RowID'] = "P" + "|"
+                                                                                + item["DocumentType"] + "|"
+                                                                                + item["Date"] + "|"
+                                                                                + item["Number"] + "|"
+                                                                                + item["Numbering"] + "|"
+                                                                                + childIndex;
                                                     for (const childKey in childItem) {
                                                         if (childItem.hasOwnProperty(childKey)) {
-                                                            row['Invoice.Payment.' + childKey] = childItem[childKey][0];
+                                                            row['PaymentRow.' + childKey] = childItem[childKey][0];
                                                         }
                                                     }
                                                     documents.push(row);
@@ -73,15 +81,52 @@ module.exports = (context, req) => {
                                                 for (let childIndex = 0; childIndex < childItems.length; childIndex++) {
                                                     let row = Object.assign({}, newEl);
                                                     const childItem = childItems[childIndex];
+                                                    row['InvoiceRow.RowNumber'] = childIndex + 1;
+                                                    row['InvoiceRow.RowID'] = "R" + "|"
+                                                                                + item["DocumentType"] + "|"
+                                                                                + item["Date"] + "|"
+                                                                                + item["Number"] + "|"
+                                                                                + item["Numbering"] + "|"
+                                                                                + childIndex;
                                                     for (const childKey in childItem) {
                                                         if (childItem.hasOwnProperty(childKey)) {
                                                             if (childKey === 'VatCode') {
-                                                                row['Invoice.Row.' + childKey] = childItem[childKey][0]._;
+                                                                if (childItem[childKey][0]._ === undefined) row['InvoiceRow.' + childKey] = "";
+                                                                else row['InvoiceRow.' + childKey] = childItem[childKey][0]._;
                                                             } else {
-                                                                row['Invoice.Row.' + childKey] = childItem[childKey][0];
+                                                                row['InvoiceRow.' + childKey] = childItem[childKey][0];
                                                             }
                                                         }
                                                     }
+                                                    if (childItem.Description[0].startsWith("** Rif. Conferma d'ordine")) {
+                                                        let uDesc = childItem.Description[0].replace("** Rif. Conferma d'ordine","");
+                                                        uDesc = uDesc.trim();
+                                                        let [id, date] = uDesc.split(" del ");
+                                                        date = date.replace(":", "");
+                                                        let dates = date.split("/");
+                                                        date = "";
+                                                        for(let ind = dates.length - 1; ind >= 0; ind --)
+                                                            date += dates[ind] + '-';
+                                                        date = date.slice(0, -1);
+                                                        orderID = id;
+                                                        orderDate = date;
+                                                    } else if (childItem.Description[0].startsWith("** Rif. Doc. di trasporto")) {
+                                                        let uDesc = childItem.Description[0].replace("** Rif. Doc. di trasporto","");
+                                                        uDesc = uDesc.trim();
+                                                        let [id, date] = uDesc.split(" del ");
+                                                        date = date.replace(":", "");
+                                                        let dates = date.split("/");
+                                                        date = "";
+                                                        for(let ind = dates.length - 1; ind >= 0; ind --)
+                                                            date += dates[ind] + '-';
+                                                        date = date.slice(0, -1);
+                                                        shipID = id;
+                                                        shipDate = date;
+                                                    }
+                                                    row['InvoiceRow.OrderID'] = orderID;
+                                                    row['InvoiceRow.OrderDate'] = orderDate;
+                                                    row['InvoiceRow.ShipmentID'] = shipID;
+                                                    row['InvoiceRow.ShipmentDate'] = shipDate;
                                                     documents.push(row);
                                                 }
                                             } else {
@@ -148,4 +193,182 @@ module.exports = (context, req) => {
             });
         }
     });
+
+    // fs.readFile("24.05.19 ddt completi.DefXml", (err, data) => {
+    //     if (err) {
+    //         context.res = {
+    //             status: 500,
+    //             body: err,
+    //         };
+    //     } else {
+    //         parser.parseString(data, (err, res) => {
+    //             if (!err) {
+    //                 let temp = res.EasyfattDocuments.Company[0];
+
+    //                 let company = {};
+    //                 for (const key in temp) {
+    //                     if (temp.hasOwnProperty(key)) {
+    //                         company['Company.' + key] = temp[key][0];
+    //                     }
+    //                 }
+
+    //                 temp = res.EasyfattDocuments.Documents[0].Document;
+    //                 let documents = [];
+    //                 let orderID = '', orderDate = '', shipID = '', shipDate = '';
+
+    //                 for (let index = 0; index < temp.length; index++) {
+    //                     const item = temp[index];
+    //                     let newEl = Object.assign({}, company);
+    //                     let childItems = [];
+    //                     for (const key in item) {
+    //                         if (item.hasOwnProperty(key)) {
+    //                             switch (key) {
+    //                                 case 'CostVatCode':
+    //                                     newEl['Document.' + key] = item[key][0]._;
+    //                                     break;
+    //                                 case 'Payments':
+    //                                     if (expand === 'payments') {
+    //                                         if (item[key][0].hasOwnProperty('Payment')) {
+    //                                             childItems = item[key][0].Payment;
+    //                                             for (let childIndex = 0; childIndex < childItems.length; childIndex++) {
+    //                                                 let row = Object.assign({}, newEl);
+    //                                                 const childItem = childItems[childIndex];
+    //                                                 row['PaymentRow.RowNumber'] = childIndex + 1;
+    //                                                 row['PaymentRow.RowID'] = "P" + "|"
+    //                                                                             + item["DocumentType"] + "|"
+    //                                                                             + item["Date"] + "|"
+    //                                                                             + item["Number"] + "|"
+    //                                                                             + item["Numbering"] + "|"
+    //                                                                             + childIndex;
+    //                                                 for (const childKey in childItem) {
+    //                                                     if (childItem.hasOwnProperty(childKey)) {
+    //                                                         row['PaymentRow.' + childKey] = childItem[childKey][0];
+    //                                                     }
+    //                                                 }
+    //                                                 documents.push(row);
+    //                                             }
+    //                                         } else {
+    //                                             documents.push(newEl);
+    //                                         }
+    //                                     }
+    //                                     break;
+    //                                 case 'Rows':
+    //                                     if (expand === 'rows') {
+    //                                         if (item[key][0].hasOwnProperty('Row')) {
+    //                                             childItems = item[key][0].Row;
+    //                                             for (let childIndex = 0; childIndex < childItems.length; childIndex++) {
+    //                                                 let row = Object.assign({}, newEl);
+    //                                                 const childItem = childItems[childIndex];
+    //                                                 row['InvoiceRow.RowNumber'] = childIndex + 1;
+    //                                                 row['InvoiceRow.RowID'] = "R" + "|"
+    //                                                                             + item["DocumentType"] + "|"
+    //                                                                             + item["Date"] + "|"
+    //                                                                             + item["Number"] + "|"
+    //                                                                             + item["Numbering"] + "|"
+    //                                                                             + childIndex;
+    //                                                 for (const childKey in childItem) {
+    //                                                     if (childItem.hasOwnProperty(childKey)) {
+    //                                                         if (childKey === 'VatCode') {
+    //                                                             if (childItem[childKey][0]._ === undefined) row['InvoiceRow.' + childKey] = "";
+    //                                                             else row['InvoiceRow.' + childKey] = childItem[childKey][0]._;
+    //                                                         } else {
+    //                                                             row['InvoiceRow.' + childKey] = childItem[childKey][0];
+    //                                                         }
+    //                                                     }
+    //                                                 }
+    //                                                 if (childItem.Description[0].startsWith("** Rif. Conferma d'ordine")) {
+    //                                                     let uDesc = childItem.Description[0].replace("** Rif. Conferma d'ordine","");
+    //                                                     uDesc = uDesc.trim();
+    //                                                     let [id, date] = uDesc.split(" del ");
+    //                                                     date = date.replace(":", "");
+    //                                                     let dates = date.split("/");
+    //                                                     date = "";
+    //                                                     for(let ind = dates.length - 1; ind >= 0; ind --)
+    //                                                         date += dates[ind] + '-';
+    //                                                     date = date.slice(0, -1);
+    //                                                     orderID = id;
+    //                                                     orderDate = date;
+    //                                                 } else if (childItem.Description[0].startsWith("** Rif. Doc. di trasporto")) {
+    //                                                     let uDesc = childItem.Description[0].replace("** Rif. Doc. di trasporto","");
+    //                                                     uDesc = uDesc.trim();
+    //                                                     let [id, date] = uDesc.split(" del ");
+    //                                                     date = date.replace(":", "");
+    //                                                     let dates = date.split("/");
+    //                                                     date = "";
+    //                                                     for(let ind = dates.length - 1; ind >= 0; ind --)
+    //                                                         date += dates[ind] + '-';
+    //                                                     date = date.slice(0, -1);
+    //                                                     shipID = id;
+    //                                                     shipDate = date;
+    //                                                 }
+    //                                                 row['InvoiceRow.OrderID'] = orderID;
+    //                                                 row['InvoiceRow.OrderDate'] = orderDate;
+    //                                                 row['InvoiceRow.ShipmentID'] = shipID;
+    //                                                 row['InvoiceRow.ShipmentDate'] = shipDate;
+    //                                                 documents.push(row);
+    //                                             }
+    //                                         } else {
+    //                                             documents.push(newEl);
+    //                                         }
+    //                                     }
+    //                                     break;
+    //                                 default:
+    //                                     newEl['Invoice.' + key] = item[key][0];
+    //                                     break;
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+
+    //                 const csvHeader = Object.keys(documents[0]).map((value) => {
+    //                     return {
+    //                         'id': value,
+    //                         'title': value,
+    //                     };
+    //                 });
+
+    //                 const csvWriter = createCsvWriter({
+    //                     path: 'output.csv',
+    //                     header: csvHeader,
+    //                 });
+
+    //                 csvWriter.writeRecords(documents)
+    //                 .then(() => {
+    //                     fs.readFile('output.csv', function(err,data){
+    //                         if (!err) {
+    //                             context.res = {
+    //                                 status: 200,
+    //                                 headers: {
+    //                                     "Content-Type": "text/csv",
+    //                                     "Content-Disposition": 'attachment;filename="'+outFilename+'"'
+    //                                 },
+    //                                 body: new Uint8Array(data)
+    //                             };
+    //                             context.done();
+    //                         } else {
+    //                             context.res = {
+    //                                 status: 500,
+    //                                 body: err.toString(),
+    //                             };
+    //                             context.done();
+    //                         }
+    //                     });
+    //                 })
+    //                 .catch((res) => {
+    //                     context.res = {
+    //                         status: 500,
+    //                         body: res.toString(),
+    //                     };
+    //                     context.done();
+    //                 });
+    //             } else {
+    //                 context.res = {
+    //                     status: res.statusCode,
+    //                     body: err,
+    //                 };
+    //                 context.done();
+    //             }
+    //         });
+    //     }
+    // });
 };
